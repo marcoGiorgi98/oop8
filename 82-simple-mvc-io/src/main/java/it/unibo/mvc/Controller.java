85c2c6b0f59826b89b496461110972b70d699501
unknown <marco.giorgi1998@gmail.com>
2022-11-18 10:09:53 +0100

    aa

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,45 @@
 package it.unibo.mvc;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+public class Controller {           //fatto io
+    File file;
+    
+    public Controller(File file) {
+        this.file=file;
+
+    }
+    public void setFile(String filee){
+        this.file=new File(filee);
+        
+    }
+    public File getFile(){
+        return this.file;
+    }
+    public String pathfile(File percorso){
+        return percorso.getPath();
+    }
+    public void scrivi(String testo) throws IOException{
+        FileWriter myWriter = new FileWriter(this.getFile());
+      myWriter.write(testo);
+        myWriter.close();
+        
+            
+        
+        
+        
+
+    }
+    
 
 }
